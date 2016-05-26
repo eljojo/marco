5ff7aeb0ca07d2d69e6c5ef4f36329c12e9c0ece
José Tomás Albornoz <jojo@eljojo.net>
2016-05-25 22:47:40 -0400

    User: twitter_id has a unique index

--- priv/repo/migrations/20160525230727_create_user.exs
+++ priv/repo/migrations/20160525230727_create_user.exs
@@ -4,15 +4,15 @@ defmodule Marco.Repo.Migrations.CreateUser do
   def change do
     create table(:users) do
       add :name, :string
-      add :twitter_handle, :string
-      add :twitter_id, :bigint
-      add :twitter_access_token, :string
-      add :twitter_access_secret, :string
+      add :twitter_handle, :string, null: false
+      add :twitter_id, :bigint, null: false
+      add :twitter_access_token, :string, null: false
+      add :twitter_access_secret, :string, null: false
       add :twitter_avatar, :string
 
       timestamps
     end
 
-    create index(:users, [:twitter_id])
+    create index(:users, [:twitter_id], unique: true)
   end
 end
